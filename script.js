dfc138a97dcc2e9ca1e5ecfe77089efbff4f8fba
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2025-07-23 22:33:54 +1200

    slight adjustments

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const profile = document.getElementById("profile");
 const titleName = document.querySelectorAll(".title");
 const currentHours = document.querySelectorAll(".current-hours");
 const previousHours = document.querySelectorAll(".previous-hours");
+const btns = document.querySelectorAll(".btn");
+const stats = document.querySelectorAll(".stats");
 
 titleName.forEach((element, index) => {
   fetch("data.json")
@@ -33,4 +36,21 @@ previousHours.forEach((element, index) => {
       const previous = data[index].timeframes.weekly.previous;
       element.textContent = ` Last Week - ${previous}hrs`;
     });
+});
+
+profile.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove active from other buttons
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+    });
+    // hide other stats
+    stats.forEach((stat) => {
+      stat.classList.remove("active");
+    });
+const element = document.querySelector(`.${id}.stats`);
+    element.classList.add("active");
+  }
 });
